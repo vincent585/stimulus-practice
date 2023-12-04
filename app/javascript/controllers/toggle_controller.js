bd995b967d28fceff0e26fb2692cf89fa6446f2e
Vincent Klimjack <vklimjack@gmail.com>
2023-12-04 16:39:20 +0000

    add chaseMe function in toggle controller

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,9 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static targets = [ "chase" ]
     static classes = [ "toggle" ]
+    static values = { index: Number }
 
     show() {
         this.element.classList.add(this.toggleClass)
+    }
+
+    chaseMe() {
+        if (this.indexValue === this.chaseTargets.length - 1) {
+            this.indexValue = 0
+        }
+        else {
+            this.indexValue++
+        }
+    }
+
+    indexValueChanged() {
+        this.showCurrent()
+    }
+
+    showCurrent() {
+        this.chaseTargets.forEach((element, index) => {
+            element.hidden = index !== this.indexValue
+        })
     }
 }
